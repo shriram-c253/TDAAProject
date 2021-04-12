c804f87ed5a543a7f7a033aa7261c58873dfd375
sarthak-kaps <sarthak.kaps2000@gmail.com>
2021-04-12 16:32:57 +0530

    Added main program, Incomplete

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "delta_star_plus_one.hpp"
+
+int main(int argc, char** argv) {
+  // take in a dataset from command line and construct a graph
+  // .. some function does this
+  
+  // call delta* + 1 algorithm on the graph
+  // spanning_tree = delta_star_plus_one(graph)
+  
+  // do further analysis
+}
