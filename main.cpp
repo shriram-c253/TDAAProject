226a5d2763db3039e0518897f61d07f7156b4341
Arjun Bharat <arjunbharat99@gmail.com>
2021-05-17 01:39:46 +0530

    Change after euclidean tests

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "delta_star_plus_one.hpp"
 #include "delta_star_plus_one_wtd.hpp"
 #include <fstream>
-
+#include <cmath>
+#include "Kruskals.hpp"
+#include "Prims.hpp"
 constexpr double eps = 1e-7;
 
 int main(int argc, char *argv[]) {
@@ -9,18 +11,26 @@ int main(int argc, char *argv[]) {
   std::fstream fin(argv[1], std::ios::in);
   std::map<int, std::set<int>> edges;
   std::map<std::pair<int, int>, double> weights;
+  //std::map<int,std::map<int,double>> weighted_edges;
   int N;
   fin >> N;
+  int edg_cnt = 0;
   for (int i = 0; i < N; ++i) {
     for (int j = 0; j < N; ++j) {
       double W;
       fin >> W;
       if (W > eps) {
+        edg_cnt++;
         edges[i].insert(j);
-        edges[j].insert(i);
-        weights[{j, i}] = W;
-        weights[{i, j}] = W;
+        weights[std::make_pair(i,j)] = W;
       }
+      /*
+      int present;
+      fin >> present;
+      if(present == 1)
+      {
+        edges[i].insert(j);//adjacency list.
+      }*/
     }
   }
   fin.close();
@@ -34,19 +44,26 @@ int main(int argc, char *argv[]) {
     cout << endl;
   }
   */
-  std::map<int, std::set<int>> bMST = delta_star_plus_one_wtd(edges, weights);
-  std::fstream fout("result.txt", std::ios::out);
+  std::map<int,std::set<int>> graph = delta_star_plus_one_wtd(edges,weights);
+  //std::pair<double,std::map<int, std::map<int,double>>>* kruskal_MST = 
+  //std::fstream fout("result.txt", std::ios::out);
+  // if(kruskal_MST == NULL)
+  // {
+  //   std::cout<<"-1";//invalid tree 
+  //   return 0;
+  // }
   int count = 0;
   int max_degree = 0;
-  for (auto i : bMST) {
+  for (auto i : graph) {
     for (auto j : i.second) {
       //std::cout << i.first << " " << j << std::endl;
       assert(edges[i.first].find(j) != edges[i.first].end());
+      //assert(fabs(weighted_edges[i.first][j.first]-j.second) <= 1e-7);//verifies that the tree is valid.
       ++count;    
     }
     max_degree = std::max(max_degree, (int) i.second.size());
   }
-  std::cout << "Maximum Degree " << max_degree << std::endl;
+  std::cout<<max_degree<<" "<<edg_cnt/2;
   assert(count / 2 == N - 1);
   return 0;
 }
--- test_script.bash
+++ test_script.bash
@@ -1,25 +1,39 @@
 make clean
 make
 result_array=()
-for graph_folder in wtd/*
+for graph_folder in euwtd/*
 do
     folder_name=$(echo $graph_folder | cut --delimiter="/" --fields=2)
     count=${folder_name#"graph"}
+    vertex_count=0
+    avg_edge_cnt=0
     running_times=()
+    max_d=0 #maximum degree. Take maximum here.
+    num_pert=0 
     echo "Running on folder $graph_folder"
     for file in $graph_folder/*
     do
-        echo "Running on graph $file"
-        a1=`date +%s.%N`
-        exec_string=$(./bmst $file)
-        a2=`date +%s.%N`
-        validity=$(echo "$exec_string != -1" | bc -l)
-        #echo $validity
-        if [[ $validity == 1 ]] 
+        if [[ $vertex_count == 0 ]] 
         then
-            time_taken=$(echo "$a2-$a1" | bc -l)
-            running_times+=($time_taken)
+            vertex_count=$(head -1 $file)
         fi
+        echo "Running on graph $file"
+        num_pert=$((num_pert+1))
+        a1=`date +%s.%N`
+        output=($(./bmst $file))
+        declare -p output #is now an array
+        a2=`date +%s.%N`
+        max_d=$(echo $((output[0]>max_d?output[0]:max_d)))
+        avg_edge_cnt=$((avg_edge_cnt+output[1]))
+        time_taken=$(echo "$a2-$a1" | bc -l)
+        running_times+=($time_taken)
+        #validity=$(echo "$exec_string != -1" | bc -l)
+        #echo $validity
+        #if [[ $validity == 1 ]] 
+        #then
+            #time_taken=$(echo "$a2-$a1" | bc -l)
+            #running_times+=($time_taken)
+        #fi
     done
     avg=0
     total_size=${#running_times[*]}
@@ -36,10 +50,11 @@ do
             std=$(echo "$std + ($avg-$x)*($avg-$x)" | bc -l)
         done
         std=$(echo "$std / $total_size" | bc -l)
-        result=$(echo "|$count|$avg|$std|$total_size|")
+        avg_edge_cnt=$(echo "$avg_edge_cnt / $num_pert" | bc -l)
+        result=$(echo "|$count|$vertex_count|$avg_edge_cnt|$max_d|$avg|$std|")
         result_array+=($result)
     else
-        result=$(echo "|$count|0|0|26|")
+        result=$(echo "|$count||0|0|26|")
         result_array+=($result)
     fi
 done
