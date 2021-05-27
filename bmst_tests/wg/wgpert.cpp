3855e1a1bcdd3e24f355d0be690aa0fd79ceede6
shriram-c253 <shriramchandran253@gmail.com>
2021-05-27 20:36:00 +0530

    fixed bug + Added updated graphs

--- bmst_tests/wg/wgpert.cpp
+++ bmst_tests/wg/wgpert.cpp
@@ -1,10 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-int main(int argc, char *argv[]) 
-{
+int main(int argc, char *argv[]) {
   int n = stoi(argv[1]);
   double p = stod(argv[2]);
   vector<vector<double>> g(n, vector<double>(n, 0));
@@ -13,19 +10,15 @@ int main(int argc, char *argv[])
   default_random_engine gen(
       std ::chrono ::system_clock ::now().time_since_epoch().count());
   double L = dis(gen), R = dis(gen);
-  if (L > R) 
-  {
+  if (L > R) {
     swap(L, R);
   }
   uniform_real_distribution<double> distw(L, R);
-  for (int i = 0; i < n; ++i) 
-  {
-    for (int j = i + 1; j < n; ++j) 
-    {
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
       double w = distw(gen);
       int yes = dist(gen);
-      if (yes) 
-      {
+      if (yes) {
         g[i][j] = g[j][i] = w;
       }
     }
@@ -33,34 +26,25 @@ int main(int argc, char *argv[])
   string name = "orig_" + string(argv[3]);
   fstream fout(name.c_str(), ios::out);
   fout << n << "\n";
-  for (int i = 0; i < n; ++i) 
-  {
-    for (int j = 0; j < n; ++j) 
-    {
-        assert(g[i][j]==g[j][i]);
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
       fout << g[i][j] << " \n"[j == n - 1];
     }
   }
   fout.close();
   int N = stoi(argv[4]);
-  for (int _N = 0; _N < N; ++_N) 
-  {
+  for (int _N = 0; _N < N; ++_N) {
     vector<vector<double>> _g(n, vector<double>(n, 0));
-    if (_N % 2) 
-    {
+    if (_N % 2) {
       double _p = 6.0 / N;
       bernoulli_distribution _dist(p);
-      for (int i = 0; i < n; ++i) 
-      {
-        for (int j = i + 1; j < n; ++j) 
-        {
+      for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
           int ok = _dist(gen);
           double w = distw(gen);
-          if (ok) 
-          {
+          if (ok) {
             _g[i][j] = _g[j][i] = (g[i][j] == 0 ? w : 0);
-          } else 
-          {
+          } else {
             _g[i][j] = _g[j][i] = g[i][j];
           }
         }
@@ -68,10 +52,9 @@ int main(int argc, char *argv[])
     } else {
       normal_distribution<double> distp(0, 0.04);
       for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
+        for (int j = i + 1; j < n; ++j) {
           if (g[i][j] > 0) {
-            _g[i][j] = g[i][j] + distp(gen);
-            _g[i][j] = _g[j][i];
+            _g[i][j] = _g[j][i] = g[i][j] + distp(gen);
           }
         }
       }
@@ -81,7 +64,6 @@ int main(int argc, char *argv[])
     fout << n << "\n";
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < n; ++j) {
-          assert(_g[i][j]==_g[j][i]);
         fout << _g[i][j] << " \n"[j == n - 1];
       }
     }
